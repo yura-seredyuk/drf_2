0779cc6e4236869653763433982144b7b6cce0a6
Yurii <yura2590@gmail.com>
2021-08-13 15:07:55 +0300

    Fix redex street validation

--- app/myapi/validator.py
+++ app/myapi/validator.py
@@ -21,7 +21,7 @@ class AddressValidator:
         elif "city" in attrs and  len(attrs["city"]) < 3:
             message = 'must contain more letters'
             self.raize_error(attrs, 'city', message)
-        elif "street" in attrs and  len(re.findall(r'([A-z0-9]+ str\. [^0][A-z0-9]+$)',attrs["street"])) != 1:
+        elif "street" in attrs and  len(re.findall(r'([A-z0-9- `]+ str\. [^0][A-z0-9]+$)',attrs["street"])) != 1:
             message = 'wrong street format. It must be like: Soborna str. 16'
             self.raize_error(attrs, 'street', message)
         elif "apartament" in attrs and  attrs['apartament'] == 0:
